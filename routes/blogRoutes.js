555e77d3e77e4839a9ef43b571cc654184a945eb
Deepankar <deepankar080892@gmail.com>
2020-06-08 20:54:06 +0200

    commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -22,8 +22,11 @@ module.exports = (app) => {
 
     const cachedBlogs = await client.get(req.user.id);
     if (cachedBlogs) {
+      console.log("serving from cache");
       return res.send(JSON.parse(cachedBlogs));
     }
+
+    console.log("serving from mongodb");
     const blogs = await Blog.find({ _user: req.user.id });
     res.send(blogs);
     client.set(req.user.id, JSON.stringify(blogs));
